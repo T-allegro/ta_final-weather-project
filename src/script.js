189167d4b5936eedb1a30c3b1b22bbd6fd567d35
T-allegro <78311737+T-allegro@users.noreply.github.com>
2021-03-30 15:51:56 +0100

    Added real time_date

--- src/script.js
+++ src/script.js
@@ -1,3 +1,23 @@
+function formatDate(timestamp) {
+    let currentDate=new Date(timestamp);
+    let date = currentDate.getDate();
+    let hours=currentDate.getHours();
+        if (hours < 10) {
+            hours=`0${hours}`;
+        }
+    let minutes=currentDate.getMinutes();
+        if (minutes < 10) {
+            minutes=`0${minutes}`;
+        }
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    let day = days[currentDate.getDay()];
+    let months = ["Jan", "Fev", "Mar", "Apr", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    let month = months[currentDate.getMonth()];
+
+    return `${day}, ${date} ${month} ${hours}:${minutes}`;
+
+}
+
 function displayTemperature(response) {
     let temperatureElement=document.querySelector("#temperature");
     temperatureElement.innerHTML=Math.round(response.data.main.temp);
@@ -11,6 +31,8 @@ function displayTemperature(response) {
     windElement.innerHTML=Math.round(response.data.wind.speed);
     let humidityElement=document.querySelector("#humidity");
     humidityElement.innerHTML=Math.round(response.data.main.humidity);
+    let dateElement=document.querySelector("#date");
+    dateElement.innerHTML=formatDate(response.data.dt * 1000);
 }
 
 
