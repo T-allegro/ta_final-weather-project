f0f9002a9e84a7bf98f19e9e8e348a1abbc30fe2
T-allegro <78311737+T-allegro@users.noreply.github.com>
2021-03-30 16:25:56 +0100

    Improved JS_updated weather icon

--- src/script.js
+++ src/script.js
@@ -20,24 +20,29 @@ function formatDate(timestamp) {
 
 function displayTemperature(response) {
     let temperatureElement=document.querySelector("#temperature");
-    temperatureElement.innerHTML=Math.round(response.data.main.temp);
     let cityElement=document.querySelector("#city");
-    cityElement.innerHTML=response.data.name.toUpperCase();
     let descriptionElement=document.querySelector("#description");
-    descriptionElement.innerHTML=response.data.weather[0].description;
     let feelsLikeElement=document.querySelector("#realfeel");
-    feelsLikeElement.innerHTML=Math.round(response.data.main.feels_like);
     let windElement=document.querySelector("#wind");
-    windElement.innerHTML=Math.round(response.data.wind.speed);
     let humidityElement=document.querySelector("#humidity");
-    humidityElement.innerHTML=Math.round(response.data.main.humidity);
     let dateElement=document.querySelector("#date");
+    let iconElement=document.querySelector("#icon");
+
+    temperatureElement.innerHTML=Math.round(response.data.main.temp);
+    cityElement.innerHTML=response.data.name.toUpperCase();
+    descriptionElement.innerHTML=response.data.weather[0].description;
+    feelsLikeElement.innerHTML=Math.round(response.data.main.feels_like);
+    windElement.innerHTML=Math.round(response.data.wind.speed);
+    humidityElement.innerHTML=Math.round(response.data.main.humidity);
     dateElement.innerHTML=formatDate(response.data.dt * 1000);
+    iconElement.setAttribute ("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    iconElement.setAttribute ("alt", response.data.weather[0].description);
 }
 
 
 let apiKey = "f2307cbce532cfdeb3168c7d625e3421";
+let city="Matosinhos";
 let units="metric";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Matosinhos&appid=${apiKey}&units=${units}`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
 
 axios.get(apiUrl).then(displayTemperature);
