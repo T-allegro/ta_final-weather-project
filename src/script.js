5f146bf4dd967bb69ff005f0d0cf0ac9b5a8dcad
T-allegro <78311737+T-allegro@users.noreply.github.com>
2021-03-30 15:08:54 +0100

    Added JS

--- src/script.js
+++ src/script.js
@@ -1,1 +1,15 @@
-let apiKey="f2307cbce532cfdeb3168c7d625e3421";
+function displayTemperature(response) {
+    let temperatureElement=document.querySelector("#temperature");
+    temperatureElement.innerHTML=Math.round(response.data.main.temp);
+    let cityElement=document.querySelector("#city");
+    cityElement.innerHTML=response.data.name.toUpperCase();
+    let descriptionElement=document.querySelector("#description");
+    descriptionElement.innerHTML=response.data.weather[0].description;
+}
+
+
+let apiKey = "f2307cbce532cfdeb3168c7d625e3421";
+let units="metric";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Matosinhos&appid=${apiKey}&units=${units}`;
+
+axios.get(apiUrl).then(displayTemperature);
